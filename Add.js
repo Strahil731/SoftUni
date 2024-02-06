c2ad4a644a51d29dfc517e90dc04192260731697
Strahil Dimitrov <hristovstrahil5@gmail.com>
2024-02-06 10:50:01 +0200

    Create Add.js

---
+++ Add.js
@@ -0,0 +1,21 @@
+function solution(input) {
+    let sum = input;
+
+    function sumNumber(number) {
+        sum = input + number;
+
+        return sum;
+    }
+
+    return sumNumber;
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+console.log('-------');
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3))
